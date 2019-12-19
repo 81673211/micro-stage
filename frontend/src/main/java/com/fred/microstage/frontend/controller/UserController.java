1e7743fc672c53b6266ad36840e3a2f4f5855fdc
sil.zhou <81673211@qq.com>
2019-12-19 17:04:45 +0800

    add module frontend

---
+++ frontend/src/main/java/com/fred/microstage/frontend/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.fred.microstage.frontend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/hi")
+    public String getBrief(@RequestParam("name") String name) {
+        return String.format("user name : %s, welcome to micro-stage made by fred", name);
+    }
+}
