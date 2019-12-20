4d70d9656e5b304f7dcaad3fb6b90cb4eadfddd4
sil.zhou <81673211@qq.com>
2019-12-20 17:25:07 +0800

    add config

---
+++ config/config-config/src/main/java/com/fred/microstage/config/config/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package com.fred.microstage.config.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ *
+ * <b>Description:.</b><br> 
+ * @author <b>sil.zhou</b>
+ * <br><b>ClassName:</b> 
+ * <br><b>Date:</b> 2019/4/12 16:13
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
