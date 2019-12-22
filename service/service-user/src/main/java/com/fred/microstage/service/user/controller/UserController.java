0f5d8af994572e4b080a9dde281afa8a179be93a
fred <81673211@qq.com>
2019-12-22 22:14:56 +0800

    problem: spring-cloud-gateway loadbalance config

--- service/service-user/src/main/java/com/fred/microstage/service/user/controller/UserController.java
+++ service/service-user/src/main/java/com/fred/microstage/service/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.fred.microstage.service.user.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping("/profile")
     public String getProfile(@RequestParam("name") String name) {
-        return String.format("user name : %s, welcome to micro-stage made by fred", name);
+        return String.format("user name : %s, welcome to micro-stage made by fred, port : %s", name, port);
     }
 }
